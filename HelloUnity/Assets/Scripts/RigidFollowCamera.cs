23e6387b35649b5e074c97e62886be51d7dfdc98
louis-jingzhe-jiang <123016687+louis-jingzhe-jiang@users.noreply.github.com>
2024-10-01 14:24:57 -0400

    rigid camera done

--- HelloUnity/Assets/Scripts/RigidFollowCamera.cs
+++ HelloUnity/Assets/Scripts/RigidFollowCamera.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class RigidFollowCamera : MonoBehaviour
 {
+    public Transform target; // following target
+
+    public float hDist; // horizontal follow distance
+    
+    public float vDist; // vertical follow distance
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   // When dealing with position, we move the camera towards the back
+        // of the model by hDist, and then move the camera towards the up
+        // direction of the model by vDist
+        transform.position = target.position - 
+            target.forward * hDist + target.up * vDist;
+        // When dealign with rotation, we want the camera to point at the 
+        // displacement vector pointing from the camera's updated position
+        // to the target's position
+        // The vector (0, hDist/3, 0) is a suppliment vector used to make the
+        // camera focus on the position slightly higher than the model's 
+        // center, since the head is usually above the model's center
+        transform.rotation = Quaternion.LookRotation(target.position - 
+            transform.position + new Vector3(0, hDist/3, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = target.position - 
+            target.forward * hDist + target.up * vDist;
+        transform.rotation = Quaternion.LookRotation(target.position - 
+            transform.position + new Vector3(0, hDist/3, 0));
         
     }
 }
--- HelloUnity/Assets/Scripts/SpringFollowCamera.cs
+++ HelloUnity/Assets/Scripts/SpringFollowCamera.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class SpringFollowCamera : MonoBehaviour
 {
+    public Transform target; // following target
+
+    public float hDist; // horizontal follow distance
+    
+    public float vDist; // vertical follow distance
+
+    public float springConst; // spring constant
+
+    public float dampConst; // damping constant
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   // same as the Rigid Follow Camera startup, move the camera to the 
+        // desired location behind the model
+        transform.position = target.position - 
+            target.forward * hDist + target.up * vDist;
+        transform.rotation = Quaternion.LookRotation(target.position - 
+            transform.position + new Vector3(0, hDist/3, 0));
     }
 
     // Update is called once per frame
