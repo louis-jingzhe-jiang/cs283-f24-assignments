2004ba81ed6e31222d87a30c4347ceac0f82fe93
louis-jingzhe-jiang <ljiang1@haverford.edu>
2024-10-20 19:02:52 -0400

    partly done

--- HelloUnity/Assets/Scripts/GazeController.cs
+++ HelloUnity/Assets/Scripts/GazeController.cs
@@ -13,31 +13,6 @@ public class GazeController : MonoBehaviour
     // downward.
     public Transform head;
     public Transform target;
-    /**
-     * The look direction
-     */
-    //private Vector3 _direction;
-    /**
-     * vector from head to target CROSS forward direction of head
-     */
-    //private Vector3 _crossProd;
-    /**
-     * vector from head to target DOT forward direction of head
-     */
-    //private float _dotProd;
-    /**
-     * vector from head to target DOT itself
-     */
-    //private float _selfDotProd;
-    /**
-     * rotation angle
-     */
-    //private float _angle;
-    /**
-     * rotation axis
-     */
-    //private Vector3 _axis;
-    //private Quaternion _rotationSuppliment;
 
     // Start is called before the first frame update
     void Start()
@@ -49,21 +24,25 @@ public class GazeController : MonoBehaviour
     void Update()
     {   
         // calculate the direction vector
-        Vector3 _direction = target.position - head.position;
+        Vector3 direction = target.position - head.position;
+        // calculate the current heading vector r (current looking object)
+        Vector3 r = head.up; // the up direction actually points forward
+        // calculate the displacement vector e representing the displacement
+        // from the current looking object to the target
+        Vector3 e = direction - r;
         // visualize the line
         Debug.DrawLine(head.position, target.position, Color.red);
         // calculating dot and cross products
-        // the head's up direction is actually pointing forward, thus we use
-        // head.up all the time in the calculation of dot and cross products
-        Vector3 _cross = Vector3.Cross(_direction, head.up);
-        float _dot = Vector3.Dot(_direction, head.up);
-        float _selfDot = Vector3.Dot(_direction, _direction);
+        Vector3 cross = Vector3.Cross(r, e);
+        if (cross.magnitude == 0) return;
+        float dot = Vector3.Dot(r, e);
+        float selfDot = Vector3.Dot(r, r);
         // calculating angle
-        float _angle = Mathf.Atan2(_cross.magnitude, _dot + _selfDot);
+        float angle = Mathf.Atan2(cross.magnitude, dot + selfDot);
         // calculating rotation axis
-        Vector3 _axis = _cross / _cross.magnitude;
+        Vector3 axis = cross / cross.magnitude;
         // Now, we should assign the rotation to the head
-        head.parent.rotation = head.parent.rotation * 
-            Quaternion.AngleAxis(_angle * Mathf.Rad2Deg, _axis);
-    }// * Quaternion.Euler(-90, 0, 90) * Quaternion.Euler(90, -90, 0)
+        head.parent.rotation *=  Quaternion.AngleAxis(
+            angle * Mathf.Rad2Deg, axis);
+    }
 }
--- HelloUnity/Assets/Scripts/TwoLinkController.cs
+++ HelloUnity/Assets/Scripts/TwoLinkController.cs
@@ -20,6 +20,18 @@ public class TwoLinkController : MonoBehaviour
      * The distance between the target and the 2nd joint
      */
     private float _targetToShoulder;
+    /** 
+     * The initial quaternion rotation of the elbow before the game starts
+     */
+    private Quaternion _elbowInitialRotation;
+    /** 
+     * The initial quaternion rotation of the shoulder before the game starts
+     */
+    private Quaternion _shoulderInitialRotation;
+    /** 
+     * The initial position of the shoulder before the game starts
+     */
+    private Vector3 _shoulderInitialPos;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +42,9 @@ public class TwoLinkController : MonoBehaviour
         Vector3 elbowToShoulder = end.parent.position - 
             end.parent.parent.position;
         _upperArmLength = elbowToShoulder.magnitude;
+        _elbowInitialRotation = end.parent.rotation;
+        _shoulderInitialRotation = end.parent.parent.rotation;
+        _shoulderInitialPos = end.parent.parent.position;
     }
 
     // Update is called once per frame
@@ -39,10 +54,12 @@ public class TwoLinkController : MonoBehaviour
         Vector3 targetToShoulderV = target.position - 
             end.parent.parent.position;
         _targetToShoulder = targetToShoulderV.magnitude;
+        Debug.Log("shoulder-target: " + _targetToShoulder);
+        Debug.Log("sum of bones: " + (_upperArmLength + _lowerArmLength));
         // solve for the elbow rotation that makes the distance between the 
         // shoulder and the end match _targetToEnd
         float elbowAng;
-        if (_upperArmLength + _lowerArmLength >= _targetToShoulder) 
+        if (_upperArmLength + _lowerArmLength <= _targetToShoulder) 
         {   // beyond reach, set elbow to 180 degrees
             elbowAng = Mathf.PI;
         }
@@ -53,13 +70,12 @@ public class TwoLinkController : MonoBehaviour
                 Mathf.Pow(_targetToShoulder, 2)) /
                 (2 * _upperArmLength * _lowerArmLength);
             elbowAng = Mathf.Acos(cosElbow);
+            //Debug.Log(cosElbow);
         }
-        // now apply the elbow rotation
-        // the elbow's rotation is only around the z axis
-        end.parent.rotation = Quaternion.Euler(0, 0, 
-            (Mathf.PI - elbowAng) * Mathf.Rad2Deg);
+        
+        
         // calculate the rotation for the shoulder
-        Vector3 r = end.position - end.parent.parent.position;
+        Vector3 r = end.position - _shoulderInitialPos;
         Vector3 e = target.position - end.position;
         Vector3 cross = Vector3.Cross(r, e);
         float selfDot = Vector3.Dot(r, r);
@@ -67,7 +83,13 @@ public class TwoLinkController : MonoBehaviour
         float shoulderAng = Mathf.Atan2(cross.magnitude, selfDot + dot);
         Vector3 axis = cross / cross.magnitude;
         // now apply the shoulder rotation
-        end.parent.parent.rotation *= Quaternion.AngleAxis(shoulderAng * 
-            Mathf.Rad2Deg, axis);
+        end.parent.parent.rotation = _shoulderInitialRotation * 
+            Quaternion.AngleAxis(shoulderAng * Mathf.Rad2Deg, axis);
+        // now apply the elbow rotation
+        // the elbow's rotation is only around the z axis
+        Debug.Log(end.parent.rotation);
+        // tested! worked!
+        end.parent.rotation = _elbowInitialRotation * Quaternion.Euler(0, 0, 
+            (Mathf.PI - elbowAng) * Mathf.Rad2Deg);
     }
 }
